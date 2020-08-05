3ee39b8e7de987275d5f2ff7dfac95e33b80604d
Akash Ravi <akashkravi@gmail.com>
2020-08-05 21:29:27 +0530

    fixes dimensions and fonts
    Signed-off-by: Akash Ravi <akashkravi@gmail.com>

--- python scripts/gen.py
+++ python scripts/gen.py
@@ -3,7 +3,7 @@ import png
 from pyqrcode import QRCode
 import csv
 from datetime import date
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 
 today = date.today()
 curr_date = today.strftime("%Y-%m-%d")
@@ -15,17 +15,18 @@ with open('sku.csv', mode='r') as file:
         s = lines[0] + '_' + lines[1] + '_' + curr_date
         # print(s)
         url = pyqrcode.create(s)
-        url.png(s + '.png', scale=3)
+        url.png(s + '.png', scale=4)
 
         im = Image.open(s + '.png')
         im = im.convert("RGBA")
 
-        img = Image.new('RGB', (500, 200), color=(255, 255, 255))
+        img = Image.new('RGB', (350, 200), color=(255, 255, 255))
         d = ImageDraw.Draw(img)
-        d.text((30, 50), lines[2], fill=(0, 0, 0))
-        d.text((30, 90), lines[3], fill=(0, 0, 0))
-        d.text((30, 130), lines[4], fill=(0, 0, 0))
+        font = ImageFont.truetype("arial.ttf", 18)
+        d.text((30, 50), lines[2], fill=(0, 0, 0), font=font)
+        d.text((30, 90), lines[3], fill=(0, 0, 0), font=font)
+        d.text((30, 130), lines[4], fill=(0, 0, 0), font=font)
 
-        img.paste(im, (300, 50, 411, 161))
+        img.paste(im, (180, 30, 328, 178))
 
         img.save(s + 'pil_text.png')
