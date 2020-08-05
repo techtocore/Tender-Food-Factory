5e6aae360dff53e12ae1c932df904dd7d6c697eb
Akash Ravi <akashkravi@gmail.com>
2020-08-05 19:56:48 +0530

    adds PIL to generate stickers
    Signed-off-by: Akash Ravi <akashkravi@gmail.com>

--- python scripts/gen.py
+++ python scripts/gen.py
@@ -1,17 +1,29 @@
-import pyqrcode 
-import png 
+import pyqrcode
+import png
 from pyqrcode import QRCode
 import csv
 from datetime import date
+from PIL import Image, ImageDraw
 
 today = date.today()
 curr_date = today.strftime("%Y-%m-%d")
 
-with open('sku.csv', mode ='r') as file: 
+with open('sku.csv', mode='r') as file:
     next(file)
     csvFile = csv.reader(file)
     for lines in csvFile:
         s = lines[0] + '_' + lines[1] + '_' + curr_date
         # print(s)
         url = pyqrcode.create(s)
-        url.png(s + '.png', scale = 6)
+        url.png(s + '.png', scale=6)
+
+        im = Image.open(s + '.png')
+        im = im.convert("RGBA")
+
+        img = Image.new('RGB', (800, 300), color=(255, 255, 255))
+        d = ImageDraw.Draw(img)
+        d.text((10, 10), "Hello World", fill=(0, 0, 0))
+
+        img.paste(im, (500, 50, 722, 272))
+
+        img.save(s + 'pil_text.png')
