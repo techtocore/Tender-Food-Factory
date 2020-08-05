b2b685b9a775c66980646d289fbce96d0fa31bf5
Akash Ravi <akashkravi@gmail.com>
2020-08-05 20:16:55 +0530

    adds text fields to label output
    Signed-off-by: Akash Ravi <akashkravi@gmail.com>

--- python scripts/gen.py
+++ python scripts/gen.py
@@ -15,15 +15,17 @@ with open('sku.csv', mode='r') as file:
         s = lines[0] + '_' + lines[1] + '_' + curr_date
         # print(s)
         url = pyqrcode.create(s)
-        url.png(s + '.png', scale=6)
+        url.png(s + '.png', scale=3)
 
         im = Image.open(s + '.png')
         im = im.convert("RGBA")
 
-        img = Image.new('RGB', (800, 300), color=(255, 255, 255))
+        img = Image.new('RGB', (500, 200), color=(255, 255, 255))
         d = ImageDraw.Draw(img)
-        d.text((10, 10), "Hello World", fill=(0, 0, 0))
+        d.text((30, 50), lines[2], fill=(0, 0, 0))
+        d.text((30, 90), lines[3], fill=(0, 0, 0))
+        d.text((30, 130), lines[4], fill=(0, 0, 0))
 
-        img.paste(im, (500, 50, 722, 272))
+        img.paste(im, (300, 50, 411, 161))
 
         img.save(s + 'pil_text.png')
