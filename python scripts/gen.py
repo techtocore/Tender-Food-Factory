d6a84ed3e753fa81da2f7cc85d03d3329f479020
Akash Ravi <akashkravi@gmail.com>
2020-08-15 04:35:20 +0530

    changes dimension
    Signed-off-by: Akash Ravi <akashkravi@gmail.com>

--- python scripts/gen.py
+++ python scripts/gen.py
@@ -20,19 +20,19 @@ with open('sku.csv', mode='r') as file:
         s = lines[0] + '_' + lines[1] + '_' + curr_date
         # print(s)
         url = pyqrcode.create(s)
-        url.png(s + '_temp.png', scale=4)
+        url.png(s + '_temp.png', scale=3)
 
         im = Image.open(s + '_temp.png')
         im = im.convert("RGBA")
         os.remove(s + '_temp.png')
 
-        img = Image.new('RGB', (350, 200), color=(255, 255, 255))
+        img = Image.new('RGB', (267, 132), color=(255, 255, 255)) # 6cm x 3.5cm
         d = ImageDraw.Draw(img)
-        font = ImageFont.truetype("arial.ttf", 18)
-        d.text((30, 50), lines[2], fill=(0, 0, 0), font=font)
-        d.text((30, 90), lines[3], fill=(0, 0, 0), font=font)
-        d.text((30, 130), lines[4], fill=(0, 0, 0), font=font)
+        font = ImageFont.truetype("arial.ttf", 17)
+        d.text((15, 30), lines[2], fill=(0, 0, 0), font=font)
+        d.text((15, 60), lines[3], fill=(0, 0, 0), font=font)
+        d.text((15, 90), lines[4], fill=(0, 0, 0), font=font)
 
-        img.paste(im, (180, 30, 328, 178))
+        img.paste(im, (150, 15, 261, 126))
 
         img.save('qr/' + s + '.png')
